402b56b6a86e21a051a958ca3eeddd6e52e41bf0
Bruno Garcia <brunogarcia84@gmail.com>
2022-11-21 04:11:53 +0200

    feat: HTML done. Ready navbar

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
